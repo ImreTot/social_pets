00cd4f73789204db0de4b5c7ee450176c880f3d3
saycoron <saycorin@gmail.com>
2023-07-27 21:31:54 +0300

    Correct some bugs.

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -2,13 +2,11 @@
 import os
 from pathlib import Path
 
-from dotenv import load_dotenv
-
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'SECRET')
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = os.getenv('HOSTS', 'SECRET_HOST').split(',')
 
